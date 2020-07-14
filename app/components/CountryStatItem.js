b4fd1009762cd61295aaeee9dff6ab2c1f656a7d
Jagrutee Banda <jagruteebanda99@gmail.com>
2020-07-14 16:41:53 +0530

    added stylesheet in Covidstatitem

--- app/components/CountryStatItem.js
+++ app/components/CountryStatItem.js
@@ -1,63 +1,77 @@
 import React from 'react';
-import {View, Text, Dimensions} from 'react-native';
+import {View, Text, Dimensions, StyleSheet} from 'react-native';
 
-const { width } = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 
 const CountryStatItem = ({item, index}) => {
   return (
     <View
       key={`stat_${index}`}
-      style={{
-        flex: 1,
-        flexDirection: 'row',
-        width: width - 16,
-        height: 50,
-        alignItems: 'center',
-        backgroundColor: index === 0 ? '#ffffcc' : '#ffffff',
-      }}>
-      <View
-        style={{justifyContent: 'center', width: (width - 16) / 4, height: 50}}>
+      style={[
+        styles.countryStatItemView,
+        {
+          backgroundColor: index === 0 ? '#99ffcc' : '#1a1a1a',
+        },
+      ]}>
+      <View style={styles.countryStatCell}>
         <Text
-          style={{
-            textAlign: 'center',
-            color: index === 0 ? '#333333' : '#33cccc',
-            padding: 4,
-          }}>
+          style={[
+            styles.countryStatItemText,
+            {color: index === 0 ? '#333333' : '#33cccc'},
+          ]}>
           {item.Country}
         </Text>
       </View>
-      <View
-        style={{justifyContent: 'center', width: (width - 16) / 4, height: 50}}>
+      <View style={styles.countryStatCell}>
         <Text
-          style={{
-            textAlign: 'center',
-            color: index === 0 ? '#333333' : '#ff3300',
-          }}>
+          style={[
+            styles.countryStatItemText,
+            {color: index === 0 ? '#333333' : '#ff3300'},
+          ]}>
           {item.TotalConfirmed}
         </Text>
       </View>
-      <View
-        style={{justifyContent: 'center', width: (width - 16) / 4, height: 50}}>
+      <View style={styles.countryStatCell}>
         <Text
-          style={{
-            textAlign: 'center',
-            color: index === 0 ? '#333333' : '#00cc00',
-          }}>
+          style={[
+            styles.countryStatItemText,
+            {color: index === 0 ? '#333333' : '#00cc00'},
+          ]}>
           {item.TotalRecovered}
         </Text>
       </View>
-      <View
-        style={{justifyContent: 'center', width: (width - 16) / 4, height: 40}}>
+      <View style={styles.countryStatCell}>
         <Text
-          style={{
-            textAlign: 'center',
-            color: index === 0 ? '#333333' : '#979797',
-          }}>
+          style={[
+            styles.countryStatItemText,
+            {color: index === 0 ? '#333333' : '#979797'},
+          ]}>
           {item.TotalDeaths}
         </Text>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  countryStatItemView: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 50,
+    alignItems: 'center',
+    marginBottom: 2,
+    width: width - 16,
+  },
+  countryStatCell: {
+    justifyContent: 'center',
+    height: 50,
+    width: (width - 16) / 4,
+  },
+  countryStatItemText: {
+    fontFamily: 'SourceSansPro-Regular',
+    textAlign: 'center',
+    padding: 4,
+  },
+});
 
 export default CountryStatItem;
