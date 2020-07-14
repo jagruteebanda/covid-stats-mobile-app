5d7abb55bbd6be6aebc9c7860883e503d4707a62
Jagrutee Banda <jagruteebanda99@gmail.com>
2020-07-14 16:41:34 +0530

    added stylesheet in Appbar

--- app/components/AppBar.js
+++ app/components/AppBar.js
@@ -1,31 +1,53 @@
 import React from 'react';
-import {View, Text, Dimensions, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 // import { TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome5Pro';
 
 const {width} = Dimensions.get('window');
 
-const AppBar = ({ handleFilterPopup = f => f }) => {
+const AppBar = (props) => {
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        width,
-        height: 50,
-        backgroundColor: '#00cc99',
-      }}>
-      <View style={{padding: 8}}>
+    <View style={styles.appBarView}>
+      {/* <View style={{padding: 18, marginLeft: 8}}>
         <Text style={{fontSize: 16, opacity: 0}}>{'<'}</Text>
-      </View>
-      <Text style={{color: '#ffffff'}}>{'COVID STATS'}</Text>
-      <TouchableOpacity onPress={() => handleFilterPopup()}>
-        <View style={{padding: 8}}>
-          <Text style={{fontSize: 16}}>{'='}</Text>
-        </View>
-      </TouchableOpacity>
+      </View> */}
+      <Text
+        style={styles.appName}>
+        {'COVID-19 Stats'}
+      </Text>
+      {/* <TouchableOpacity onPress={() => handleFilterPopup()}>
+        <View style={{padding: 16, marginRight: 8, opacity: 0}}>
+          <Text style={{fontSize: 16}}>{'='}</Text> */}
+      {/* <Icon name={"sliders-h"} size={30} color="#900" /> */}
+      {/* </View>
+      </TouchableOpacity> */}
     </View>
   );
 };
+
+
+
+const styles = StyleSheet.create({
+  appBarView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width,
+    height: 50,
+    backgroundColor: '#00cc99',
+    elevation: 2,
+  },
+  appName: {
+    fontFamily: 'SourceSansPro-SemiBold',
+    fontSize: 20,
+    color: '#ffffff',
+  }
+});
 
 export default AppBar;
