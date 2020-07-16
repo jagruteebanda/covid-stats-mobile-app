4839d797432363964ee9a411cf24712339daae86
Jagrutee Banda <jagruteebanda99@gmail.com>
2020-07-16 16:18:10 +0530

    removed extra code

--- app/components/Splash.js
+++ app/components/Splash.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react';
-import {View, Text, Dimensions} from 'react-native';
+import {View, Text, Dimensions, StyleSheet, Image} from 'react-native';
 
 const {width, height} = Dimensions.get('window');
 
 export default class Splash extends Component {
-
   componentDidMount = () => {
     setTimeout(() => {
       this.props.navigation.navigate('Home');
     }, 3000);
-  }
+  };
 
   render() {
     return (
-      <View style={{flex: 1, width, height, backgroundColor: '#00cc99'}}>
-        <Text>{'splash'}</Text>
+      <View style={styles.splashView}>
+        <Image style={styles.image} source={require('../images/logo.png')} />
+        <Text style={styles.appTitle}>{'COVID-19 Stats'}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  splashView: {
+    flex: 1,
+    width,
+    height,
+    backgroundColor: '#262626',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {width: 150, height: 150},
+  appTitle: {
+    fontFamily: 'SourceSansPro-SemiBold',
+    fontSize: 20,
+    color: '#ffffff',
+    marginTop: 10,
+  },
+});
