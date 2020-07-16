dd5e29f9798af79dc939e950e2f2bc6ba7db085b
Jagrutee Banda <jagruteebanda99@gmail.com>
2020-07-16 16:16:45 +0530

    added modal side touch close function call

--- app/components/FilterModal.js
+++ app/components/FilterModal.js
@@ -9,10 +9,11 @@ import {
   Dimensions,
   TouchableOpacity,
   StyleSheet,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
-const {width, height} = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 
 const FilterModal = ({
   filterPopupOpen,
@@ -29,91 +30,99 @@ const FilterModal = ({
       animationType="fade"
       transparent={true}
       visible={filterPopupOpen}
-      onRequestClose={() => {
-        // Alert.alert('Modal has been closed.');
-      }}>
-      <View style={styles.modalCentricView}>
-        <View style={styles.modalView}>
-          <View style={styles.modalHeadingView}>
-            <Text style={styles.modalHeading}>{'Search Filter'}</Text>
-            <TouchableOpacity onPress={() => closeFilterPopup()}>
-              <View style={{marginLeft: 4}}>
-                <Icon name={'times-circle'} size={24} color={'#33ccff'} />
+      onRequestClose={() => closeFilterPopup()}>
+      <TouchableWithoutFeedback onPress={() => closeFilterPopup()}>
+        <View style={styles.modalCentricView}>
+          <TouchableWithoutFeedback onPress={() => {}}>
+            <View style={styles.modalView}>
+              <View style={styles.modalHeadingView}>
+                <Text style={styles.modalHeading}>{'Search Filter'}</Text>
+                <TouchableOpacity onPress={() => closeFilterPopup()}>
+                  <View style={{marginLeft: 4}}>
+                    <Icon name={'times-circle'} size={24} color={'#33ccff'} />
+                  </View>
+                </TouchableOpacity>
               </View>
-            </TouchableOpacity>
-          </View>
-          <View style={{width: width - 50, height: 180}}>
-            <View style={styles.columnView}>
-              <View style={styles.columnHeading}>
-                <Text style={styles.columnText}>{'Column'}</Text>
+              <View style={{width: width - 50, height: 180}}>
+                <View style={styles.columnView}>
+                  <View style={styles.columnHeading}>
+                    <Text style={styles.columnText}>{'Column'}</Text>
+                  </View>
+                  <View style={styles.pickerView}>
+                    <Picker
+                      selectedValue={filterData.column}
+                      style={styles.picker}
+                      onValueChange={(itemValue, itemIndex) =>
+                        onChangeFilterColumn(itemValue)
+                      }>
+                      {/* <Picker.Item label="Select column" value="" /> */}
+                      <Picker.Item label="Confirmed" value="TotalConfirmed" />
+                      <Picker.Item label="Recovered" value="TotalRecovered" />
+                      <Picker.Item label="Deaths" value="TotalDeaths" />
+                    </Picker>
+                  </View>
+                </View>
+                <View style={styles.columnView}>
+                  <View style={styles.columnHeading}>
+                    <Text style={styles.columnText}>{'Comparator'}</Text>
+                  </View>
+                  <View style={styles.pickerView}>
+                    <Picker
+                      selectedValue={filterData.comparator}
+                      style={styles.picker}
+                      onValueChange={(itemValue, itemIndex) =>
+                        onChangeFilterComparator(itemValue)
+                      }>
+                      {/* <Picker.Item label="Select comparator" value="" /> */}
+                      <Picker.Item label="greater than" value=">=" />
+                      <Picker.Item label="less than" value="<=" />
+                    </Picker>
+                  </View>
+                </View>
+                <View style={styles.columnView}>
+                  <View style={styles.columnHeading}>
+                    <Text style={styles.columnText}>{'No. of cases'}</Text>
+                  </View>
+                  <View
+                    style={{
+                      width: (2 * (width - 50)) / 3 - 10,
+                      height: 50,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <TextInput
+                      style={styles.textInput}
+                      keyboardType={'numeric'}
+                      onChangeText={(number) => onChangeFilterNumber(number)}
+                      value={`${filterData.number}`}
+                      placeholder={'e.g. 1000'}
+                      placeholderTextColor={'#fff'}
+                      maxLength={10}
+                    />
+                  </View>
+                </View>
               </View>
-              <View style={styles.pickerView}>
-                <Picker
-                  selectedValue={filterData.column}
-                  style={styles.picker}
-                  onValueChange={(itemValue, itemIndex) =>
-                    onChangeFilterColumn(itemValue)
-                  }>
-                  <Picker.Item label="Select column" value="" />
-                  <Picker.Item label="Confirmed" value="TotalConfirmed" />
-                  <Picker.Item label="Recovered" value="TotalRecovered" />
-                  <Picker.Item label="Deaths" value="TotalDeaths" />
-                </Picker>
+              <View style={styles.actionButtonsView}>
+                <TouchableHighlight onPress={() => resetFilter()}>
+                  <View style={styles.buttonView}>
+                    <Text style={styles.buttonText}>{'Reset'}</Text>
+                  </View>
+                </TouchableHighlight>
+                <TouchableHighlight
+                  onPress={() => handleFilterSort(filterData)}>
+                  <View
+                    style={[
+                      styles.buttonView,
+                      {backgroundColor: 'rgba(51, 204, 255, 0.2)'},
+                    ]}>
+                    <Text style={styles.buttonText}>{'Apply'}</Text>
+                  </View>
+                </TouchableHighlight>
               </View>
             </View>
-            <View style={styles.columnView}>
-              <View style={styles.columnHeading}>
-                <Text style={styles.columnText}>{'Comparator'}</Text>
-              </View>
-              <View style={styles.pickerView}>
-                <Picker
-                  selectedValue={filterData.comparator}
-                  style={styles.picker}
-                  onValueChange={(itemValue, itemIndex) =>
-                    onChangeFilterComparator(itemValue)
-                  }>
-                  <Picker.Item label="Select comparator" value="" />
-                  <Picker.Item label="greater than" value=">=" />
-                  <Picker.Item label="less than" value="<=" />
-                </Picker>
-              </View>
-            </View>
-            <View style={styles.columnView}>
-              <View style={styles.columnHeading}>
-                <Text style={styles.columnText}>{'No. of cases'}</Text>
-              </View>
-              <View
-                style={{
-                  width: (2 * (width - 50)) / 3 - 10,
-                  height: 50,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <TextInput
-                  style={styles.textInput}
-                  keyboardType={'numeric'}
-                  onChangeText={(number) => onChangeFilterNumber(number)}
-                  value={filterData.number}
-                  placeholder={'e.g. 1000'}
-                  placeholderTextColor={'#fff'}
-                />
-              </View>
-            </View>
-          </View>
-          <View style={styles.actionButtonsView}>
-            <TouchableHighlight onPress={() => resetFilter()}>
-              <View style={styles.buttonView}>
-                <Text style={styles.buttonText}>{'Reset'}</Text>
-              </View>
-            </TouchableHighlight>
-            <TouchableHighlight onPress={() => handleFilterSort(filterData)}>
-              <View style={[styles.buttonView, { backgroundColor: 'rgba(51, 204, 255, 0.2)' }]}>
-                <Text style={styles.buttonText}>{'Apply'}</Text>
-              </View>
-            </TouchableHighlight>
-          </View>
+          </TouchableWithoutFeedback>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
@@ -126,6 +135,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
   },
   modalView: {
+    // justifyContent: 'center',
+    // alignItems: 'center',
     width: width - 50,
     height: 280,
     backgroundColor: '#262626',
@@ -209,7 +220,7 @@ const styles = StyleSheet.create({
     fontFamily: 'SourceSansPro-Regular',
     textAlign: 'center',
     color: '#33ccff',
-    fontSize: 16
+    fontSize: 16,
   },
 });
 
