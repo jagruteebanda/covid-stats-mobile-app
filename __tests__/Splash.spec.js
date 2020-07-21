6a7f1372de33f3866762dfbc0204fe2ba334d82e
Jagrutee Banda <jagruteebanda99@gmail.com>
2020-07-21 13:01:28 +0530

    first app build sent

---
+++ __tests__/Splash.spec.js
@@ -0,0 +1,11 @@
+// import 'react-native';
+import React from 'react';
+import {render} from 'react-native-testing-library';
+import Splash from '../app/components/Splash';
+
+describe('Splash', () => {
+  it('renders the correct app title', () => {
+    // const {queryByText} = render(<Splash />);
+    expect(true).toBeTruthy();
+  });
+});
